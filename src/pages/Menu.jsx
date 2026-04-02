5647e5d140b7cbd3a48d4d9cd365c83362efae6f
angelfhvasquez <angelfhvasquez@gmail.com>
2026-04-02 02:15:54 +0000

    fix fetch menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -42,7 +42,7 @@ export default function Menu() {
       setLoading(true);
       setError(null);
       try {
-        const response = await fetch('/menu.json');
+        const response = await fetch('/Parcial_Final_Interfaces2/menu.json');
         if (!response.ok) throw new Error('No se puede cargar el menú');
         const data = await response.json();
         if (alive) {
