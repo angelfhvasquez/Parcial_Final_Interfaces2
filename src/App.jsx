7cb29148e4eb1bb916b0fbdf1439cb204404a7eb
angelfhvasquez <angelfhvasquez@gmail.com>
2026-04-02 02:13:05 +0000

    fix router and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from 'framer-motion';
 import { CartProvider } from './contexts/CartContext';
 import { AuthProvider } from './contexts/AuthContext';
